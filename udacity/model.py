690b5d1674b59057beb236231a6792c27ce040af
ykim9265 <ykim9265@gmail.com>
2021-02-08 00:22:01 -0500

    cleaned up code

--- udacity/model.py
+++ udacity/model.py
@@ -49,7 +49,7 @@ class Actor(nn.Module):
         self.seed = torch.manual_seed(seed)
 
         self.model = nn.Sequential(
-            nn.Linear(self.state_size, fc1_units),
+            nn.Linear(state_size, fc1_units),
             nn.BatchNorm1d(fc1_units), 
             nn.ReLU(),
             nn.Linear(fc1_units, fc2_units),
@@ -70,12 +70,6 @@ class Actor(nn.Module):
 
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
-        
-        # #x = self.normalizer(state)
-        # x = F.relu(self.fc1(state))
-        # x = F.relu(self.fc2(x))
-        # return torch.tanh(self.fc3(x))
-
         
         return torch.tanh(self.model(state))
 
@@ -107,22 +101,18 @@ class Critic(nn.Module):
         """
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
-        # self.fcs1 = nn.Linear(state_size, fcs1_units)
-        # self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
-        # self.fc3 = nn.Linear(fc2_units, 1)
-        # self.dropout = nn.Dropout(p=dropout)
-        # self.normalizer = nn.BatchNorm1d(state_size)
 
-        self.model = nn.Sequential(
-            nn.Linear(state_size, fcs1_units),
-            nn.BatchNorm1d(fcs1_units), 
-            nn.ReLU(),
-            nn.Linear(fcs1_units+action_size, fc2_units),
-            nn.BatchNorm1d(fc2_units),
-            nn.ReLU(),
-            nn.Dropout(p=dropout),
-            nn.Linear(fc2_units, 1)
-        )
+        # I would like to use a single model, but because 'action' needs to be concatenated with 'state', I am using 2 models:
+        # self.model = nn.Sequential(
+        #     nn.Linear(state_size, fcs1_units),
+        #     nn.BatchNorm1d(fcs1_units), 
+        #     nn.ReLU(),
+        #     nn.Linear(fcs1_units+action_size, fc2_units),
+        #     nn.BatchNorm1d(fc2_units),
+        #     nn.ReLU(),
+        #     nn.Dropout(p=dropout),
+        #     nn.Linear(fc2_units, 1)
+        # )
 
         self.model_1 = nn.Sequential(
             nn.Linear(state_size, fcs1_units),
@@ -144,9 +134,6 @@ class Critic(nn.Module):
         print('Updated network architecture using Sequential')
 
     def reset_parameters(self):
-        # self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
-        # self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
-        # self.fc3.weight.data.uniform_(-3e-3, 3e-3)
 
         self.model_1[0].weight.data.uniform_(*hidden_init(self.model_1[0]))
         self.model_2[0].weight.data.uniform_(*hidden_init(self.model_2[0]))
@@ -155,14 +142,6 @@ class Critic(nn.Module):
 
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
-        
-        # #x = self.normalizer(state)
-        # x = F.relu(self.fcs1(state))
-        # x = torch.cat((x, action), dim=1)
-        # x = F.relu(self.fc2(x))
-        # x = self.dropout(x)
-        
-        #return self.fc3(x)
 
         x = self.model_1(state)
 
